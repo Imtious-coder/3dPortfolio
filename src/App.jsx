86e63dc00dc417bac0300db4b660ed4204928220
Imtious-Coder <iarafat386@gmail.com>
2024-02-28 21:25:08 +0600

    Tailwind CSS added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import React from "react";
-
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-red-400">Hello world!</h1>
+    </div>
+  );
 };
 
 export default App;
