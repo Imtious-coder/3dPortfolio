67997a948c98bf7e23996baeecf1f0b44e557cc6
Imtious-Coder <iarafat386@gmail.com>
2024-02-29 14:02:28 +0600

    Island lights & colors updated.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -31,11 +31,20 @@ const Home = () => {
         camera={{ near: 0.1, far: 1000 }}
       >
         <Suspense fallback={<Loader />}>
-          <directionalLight />
-          <ambientLight />
-          <pointLight />
-          <spotLight />
-          <hemisphereLight />
+          <directionalLight position={[1, 1, 1]} intensity={2} />
+          <ambientLight intensity={0.5} />
+          <pointLight position={[10, 5, 10]} intensity={2} />
+          <spotLight
+            position={[0, 50, 10]}
+            angle={0.15}
+            penumbra={1}
+            intensity={2}
+          />
+          <hemisphereLight
+            skyColor="#b1e1ff"
+            groundColor="#000000"
+            intensity={1}
+          />
           <Island
             // isRotating={isRotating}
             // setIsRotating={setIsRotating}
