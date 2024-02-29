027ae9f7c36c36c767b0d290a1d3d221be631781
Imtious-Coder <iarafat386@gmail.com>
2024-02-29 11:36:46 +0600

    Navbar created.

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <header className="header">
+      <NavLink
+        to="/"
+        className="w-10 h-10 rounded-lg bg-white flex items-center justify-center font-bold shadow-md"
+      >
+        <p className="blue-gradient_text">IM</p>
+      </NavLink>
+      <nav className="flex text-lg gap-7 font-medium">
+        <NavLink
+          to="/about"
+          className={({ isActive }) =>
+            isActive ? "text-blue-500" : "text-black"
+          }
+        >
+          About
+        </NavLink>
+        <NavLink
+          to="/projects"
+          className={({ isActive }) =>
+            isActive ? "text-blue-500" : "text-black"
+          }
+        >
+          Projects
+        </NavLink>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
